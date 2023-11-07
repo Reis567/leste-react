85f00a3800c48788257064807215e1fb61db87f5
Reis567 <matheusdosreis9@gmail.com>
2023-11-07 20:29:29 -0300

    components import

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter , RouterProvider, Route} from 'react-router-dom'
 
+//Paginas
+import Home from './routes/Home.jsx'
+import newContato from './routes/newContato.jsx'
+
 const router = createBrowserRouter([
   {
     element:<App/>,
