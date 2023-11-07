b7533f7a4fd33c1da78740f17ca4dfccc444ea72
Reis567 <matheusdosreis9@gmail.com>
2023-11-07 20:28:27 -0300

    routes and initial components

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter , RouterProvider, Route} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    element:<App/>,
+    children:[
+      {
+        path:'/'
+      },
+      {
+        path:'/new'
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ src/routes/newContato.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const newContato = () => {
+  return (
+    <div>newContato</div>
+  )
+}
+
+export default newContato
