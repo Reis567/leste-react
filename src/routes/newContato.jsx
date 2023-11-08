d48383f1c3065bb7d3afd2c5b27fb64f533ed315
Reis567 <matheusdosreis9@gmail.com>
2023-11-08 08:16:09 -0300

    new contato page

--- src/routes/newContato.jsx
+++ src/routes/newContato.jsx
@@ -3,7 +3,58 @@ import './NewContato.css'
 
 const   NewContato = () => {
   return (
-    <div>newContato</div>
+    <div className='new-contato'>
+        <h2>
+          Criar novo contato
+        </h2>
+        <form>
+          <div className='form-control'>
+
+            <label htmlFor="first_name">Nome :</label>
+            <input type="text" name='first_name' placeholder='Insira aqui o nome' id='first_name' />
+          </div>
+
+          <div className='form-control'>
+
+            <label htmlFor="last_name">Sobrenome :</label>
+            <input type="text" name='last_name' placeholder='Insira aqui o sobrenome' id='last_name' />
+          </div>
+
+          <div className='form-control'>
+
+            <label htmlFor="email">Email :</label>
+            <input type="email" name='email' placeholder='Insira aqui o email' id='email' />
+          </div>
+
+          <div className='form-control'>
+          <label htmlFor="gender">Gênero :</label>
+          <select name='gender' id='genero'>
+            <option value="M">Masculino</option>
+            <option value="F">Feminino</option>
+            <option value="O">Outro</option>
+            <option value="N">Prefiro nâo mencionar</option>
+          </select>
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor="language">Idioma :</label>
+          <select name='language' id='idioma'>
+          <option value="Ingles">Inglês</option>
+          <option value="Espanhol">Espanhol</option>
+          <option value="Mandarim">Mandarim</option>
+          <option value="Portugues">Português</option>
+          <option value="Italiano">Italiano</option>
+          </select>
+        </div>
+
+          <div className='form-control'>
+
+            <label htmlFor="birthday">Nascimento :</label>
+            <input type="date" name='birthday' placeholder='Insira aqui a data de nascimento' id='nascimento' />
+          </div>
+          <input type="submit" value="Criar contato" className='btn'/>
+        </form>
+    </div>
   )
 }
 
