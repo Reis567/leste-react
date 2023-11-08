8af324a3991d7eb80444fd153c10e70399a38b1d
Reis567 <matheusdosreis9@gmail.com>
2023-11-07 21:10:28 -0300

    initial style of header

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,14 +7,14 @@ const NavBar = () => {
             <Link to={'/'}>
                 <img src="https://static.wixstatic.com/media/1f0d31_6973563a5a4a44a3b87e02b5e42b7bd3~mv2.png/v1/fill/w_136,h_48,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/1f0d31_6973563a5a4a44a3b87e02b5e42b7bd3~mv2.png" alt="" />
             </Link>
-        <div>
+        <div className='navlist'>
             <h3>
-                <Link to={'/'}>
+                <Link to={'/'} className='linkheader'>
                     Home
                 </Link>
             </h3>
             <h3>
-                <Link to={'/new'} className='new-btn'>
+                <Link to={'/new'} className='linkheader'>
                     Novo contato
                 </Link>
             </h3>
