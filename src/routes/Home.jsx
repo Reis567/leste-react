a1aff374a1a975fcd66032788f5042c4c03b8d3e
Reis567 <matheusdosreis9@gmail.com>
2023-11-07 21:47:40 -0300

    api of contacts get

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import './Home.css'
+import axios from "axios"
 
 const Home = () => {
+
+  const [contatos, setContatos]= useState([])
+
+  const getPosts = async() => {
+    try {
+      const response = await axios.get('https://my.api.mockaroo.com/lestetelecom/test.json?key=f55c4060')
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    getPosts()
+  },[])
   return (
     <div>Home</div>
   )
