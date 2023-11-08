e56e84f1f721b6839d0a135bd42c3c90ca96826c
Reis567 <matheusdosreis9@gmail.com>
2023-11-07 21:02:56 -0300

    reset.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+import './reset.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { createBrowserRouter , RouterProvider, Route} from 'react-router-dom'
 
 //Paginas
