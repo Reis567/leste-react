ee5c3803457054ab89edc8ae059bd5b307a5ddcd
Reis567 <matheusdosreis9@gmail.com>
2023-11-09 19:52:14 -0300

    card style

--- src/components/ContatoCard.jsx
+++ src/components/ContatoCard.jsx
@@ -6,26 +6,43 @@ import PropTypes from 'prop-types';
 
 import './ContatoCard.css'; 
 
+const formatDate = (date) => {
+  const d = new Date(date);
+  const day = d.getDate().toString().padStart(2, '0');
+  const month = (d.getMonth() + 1).toString().padStart(2, '0'); // Mês começa em 0 (janeiro é 0)
+  const year = d.getFullYear();
+  return `${day}/${month}/${year}`;
+};
+
 const ContatoCard = ({ contato,onDelete }) => {
+  const formattedBirthday = formatDate(contato.birthday);
+  const generoExibido = contato.gender === "F" ? "Feminino" : contato.gender === "M" ? "Masculino" : contato.gender;
   return (
     <div className="contatocard">
-      <button onClick={onDelete} className='delete-btn'>
-      <FontAwesomeIcon icon={faTrash} />
-      </button>
-      <Link to={`/edit/${contato.id}`}>
-        <button className='edit-btn'>
-          <FontAwesomeIcon icon={faEdit} />
-        </button>
-      </Link>
-      <img src={contato.avatar} alt="" />
+      <img src={contato.avatar} alt="avatar image" />
       <div className="name">
         <h2>{contato.first_name}</h2>
         <h2>{contato.last_name}</h2>
       </div>
-      <p>{contato.email}</p>
-      <p>{contato.gender}</p>
-      <p>{contato.language}</p>
-      <p>{contato.birthday}</p>
+      <p className='email'>{contato.email}</p>
+      <p>Gênero : <span className='genero'>{generoExibido}</span></p>
+      <p>Idioma : <span className="idioma">{contato.language}</span></p>
+      <p>Data de Nascimento: <span className="aniversario">{formattedBirthday}</span></p>
+      <hr style={{ width: '100%', color: '#333', borderStyle: 'solid',opacity:'40%' }} />
+
+      <div className='buttons'>
+
+      <button onClick={onDelete} className='delete-btn'>
+        Apagar 
+      <FontAwesomeIcon icon={faTrash} />
+      </button>
+      <Link to={`/edit/${contato.id}`}>
+        <button className='edit-btn'>
+          Editar
+          <FontAwesomeIcon icon={faEdit} />
+        </button>
+      </Link>
+      </div>
     </div>
   );
 };
